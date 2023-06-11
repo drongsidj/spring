681745f2e453945035173f014a53aded40537575
igio <mrdugu6@gmail.com>
2023-06-11 21:59:11 +0800

    添加logback日志

--- src/main/java/top/igio/template/TemplateApplication.java
+++ src/main/java/top/igio/template/TemplateApplication.java
@@ -1,12 +1,17 @@
 package top.igio.template;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TemplateApplication {
 
+	final static Logger logger = LoggerFactory.getLogger(TemplateApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("TemplateApplication start...");
 		SpringApplication.run(TemplateApplication.class, args);
 	}
 
